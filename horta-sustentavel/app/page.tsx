49c5331650b62f9a8037994ea62457b2a016478d
Nikao777 <139736216+Nicolas7720@users.noreply.github.com>
2025-05-31 00:15:18 -0300

    projeto iniciado e começo do card de plantas

---
+++ horta-sustentavel/app/page.tsx
@@ -0,0 +1,11 @@
+import Card from "./components/card";
+
+export default function Home() {
+  return (
+    <>
+      <main className="max-w-full flex flex-col justify-center items-center h-dvh">
+        <Card />
+      </main>
+    </>
+  );
+}
